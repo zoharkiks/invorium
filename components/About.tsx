abf1347e8937c2a6ec9bd43e95ddb106ca291301
zoharkiks <zoharkiks@gmail.com>
2022-07-24 00:04:26 +0530

    worked on about-mob

--- components/About.tsx
+++ components/About.tsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { images } from '../constants'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div className="flex flex-col px-6 pb-20">
+    <h1 className=" font-gilextra text-[32px] mb-2">We are Invoriem</h1>
+    <p className="font-gilmedium tracking-[1px] leading-6 text-[#1F1F1F] mb-8">
+    We fulfill international property developments
+    </p>
+    <div className="h-[338px]  w-[327px] bg-cover bg-no-repeat bg-center bg-[url('../assets/video.png')] ">
+   
+    </div>
+  </div>
   )
 }
 
