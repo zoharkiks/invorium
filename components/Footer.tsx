68c640e97cd01e881c1c819bd766052908d5e388
zoharkiks <zoharkiks@gmail.com>
2022-07-24 00:23:07 +0530

    finished footer-mob

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,8 +1,18 @@
+import { Icon } from '@iconify/react'
 import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='flex flex-col justify-center items-center'>
+      <a className='text-[#4A2AD3] font-gilmedium tracking-[1px] leading-8 mb-4' >info@invoriem.com</a>
+      <div className="flex justify-center space-x-6 text-[#1F1F1F] mb-8">
+      <Icon className='h-6 w-6'  icon="ant-design:instagram-filled" />
+      <Icon className='h-6 w-6'icon="typcn:social-facebook" />
+      <Icon className='h-6 w-6' icon="typcn:social-twitter" />
+      <Icon className='h-6 w-6' icon="typcn:social-linkedin" />
+      </div>
+      <span className="font-gilmedium text-sm tracking-[2px] leading-7">Copyright © 2020 Invoriem </span>
+    </div>
   )
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ import {About, Casestudy, Footer,Header,Navbar} from '../components'
 
 const Home: NextPage = () => {
   return (
-    <div className="">
+    <div className="pb-8">
       <Head>
         <title>Invorum</title>
         <link rel="icon" href="/favicon.ico" />
