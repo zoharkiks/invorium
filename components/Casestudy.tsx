1c6fbcac1586f6269f11847539adcfcb1d92126b
zoharkiks <zoharkiks@gmail.com>
2022-07-23 23:55:52 +0530

    worked on case study-mob

--- components/Casestudy.tsx
+++ components/Casestudy.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import { images } from "../constants";
 
 const Casestudy = () => {
   return (
-    <div>Casestudy</div>
-  )
-}
+    <div className="flex flex-col px-6 py-20">
+      <h1 className=" font-gilextra text-[32px] mb-2">Case Studies</h1>
+      <p className="font-gilmedium tracking-[1px] leading-6 text-[#1F1F1F] mb-8">
+        We deliver a unique and truly bespoke approach to delivering
+        international developments to market, with over 10 years success around
+        the world.
+      </p>
+      <div className="flex">
+        <img className="w-[224px] h-[264px]" src={images.France.src} alt="" />
+      </div>
+    </div>
+  );
+};
 
-export default Casestudy
+export default Casestudy;
--- components/Header.tsx
+++ components/Header.tsx
@@ -6,7 +6,7 @@ import { Icon } from '@iconify/react';
 const Header = () => {
   return (
     <div className="flex flex-col  ">
-      <div className="flex flex-col justify-center items-center pt-[127px] font-gilmedium tracking-[1px] text-[#1F1F1F] ">
+      <div className="flex flex-col justify-center items-center pt-[127px] font-gilmedium tracking-[1px] leading-6 text-[#1F1F1F] ">
       <img className='h-8' src={images.logodark.src} alt="" />
       <span className='w-[50%] text-center mt-7'>We fulfill international property developments</span>
       </div>
