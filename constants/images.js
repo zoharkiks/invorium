bf542e1fc1c0e96abb26e62ffe836bd27c527cbe
zoharkiks <zoharkiks@gmail.com>
2022-07-23 16:40:05 +0530

    added imports for images, cleared boiler plate

---
+++ constants/images.js
@@ -0,0 +1,9 @@
+import logo from '../assets/logo.png'
+import headerimage from '../assets/headerimage.png'
+import France from '../assets/France.png'
+import London from '../assets/London.png'
+import Moroco from '../assets/Moroco.png'
+import Portugal from '../assets/Portugal.png'
+import video from '../assets/video.png'
+
+export default{logo,headerimage,France,London, Moroco,Portugal,video}
