7fefbd2ca4f7a189bdab2b17dcc5612f8751b399
zoharkiks <zoharkiks@gmail.com>
2022-07-23 23:31:46 +0530

    added chervron

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import {images} from '../constants'
+import { Icon } from '@iconify/react';
+
 
 const Header = () => {
   return (
@@ -7,6 +9,9 @@ const Header = () => {
       <div className="flex flex-col justify-center items-center pt-[127px] font-gilmedium tracking-[1px] text-[#1F1F1F] ">
       <img className='h-8' src={images.logodark.src} alt="" />
       <span className='w-[50%] text-center mt-7'>We fulfill international property developments</span>
+      </div>
+      <div className="flex justify-center items-center mt-[500px]">
+      <Icon className='text-white w-8 h-8 animate-bounce' icon="bi:chevron-double-down" />
       </div>
 
     </div>
