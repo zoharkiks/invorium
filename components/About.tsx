9705fe35d46863f3f7b50c2c359a1e2ef26f6f96
zoharkiks <zoharkiks@gmail.com>
2022-07-24 12:33:49 +0530

    added home animations

--- components/About.tsx
+++ components/About.tsx
@@ -1,21 +1,25 @@
-import React from 'react'
-import { images } from '../constants'
+import React from "react";
+import { motion } from "framer-motion";
+
 
 const About = () => {
   return (
-    <div className="flex flex-col px-6 pb-20 lg:px-[100px]">
-    <h1 className=" font-gilextra text-[32px] lg:text-[80px] mb-2">We are Invoriem</h1>
-    <p className="font-gilmedium lg:text-2xl tracking-[1px] leading-6 text-[#5b5e5f] mb-8">
-    We fulfill international property developments
-    </p>
-    <div className="flex justify-center items-center">
+    <motion.div
+    whileInView={{ y: [100, 50, 0], opacity: [0, 0, 1] }}
+    transition={{ duration: 0.5 }}
+      className="flex flex-col px-6 pb-20 lg:px-[100px]"
+    >
+      <h1 className=" font-gilextra text-[32px] lg:text-[80px] mb-2">
+        We are Invoriem
+      </h1>
+      <p className="font-gilmedium lg:text-2xl tracking-[1px] leading-6 text-[#5b5e5f] mb-8">
+        We fulfill international property developments
+      </p>
+      <div className="flex justify-center items-center">
+        <div className="h-[338px] lg:h-[500px] w-full bg-cover bg-no-repeat bg-center bg-[url('../assets/video.png')] "></div>
+      </div>
+    </motion.div>
+  );
+};
 
-    <div className="h-[338px] lg:h-[500px] w-full bg-cover bg-no-repeat bg-center bg-[url('../assets/video.png')] ">
-    </div>
-   
-    </div>
-  </div>
-  )
-}
-
-export default About
+export default About;
--- components/Casestudy.tsx
+++ components/Casestudy.tsx
@@ -1,9 +1,13 @@
 import React from "react";
 import { images } from "../constants";
+import { motion } from 'framer-motion';
+
+
 
 const Casestudy = () => {
   return (
-    <div className="flex flex-col px-6 py-20 lg:px-[100px] lg:py-[104px] ">
+    <motion.div whileInView={{ y: [100, 50, 0], opacity: [0, 0, 1] }}
+    transition={{ duration: 0.5 }}  className="flex flex-col px-6 py-20 lg:px-[100px] lg:py-[104px] ">
       <h1 className=" font-gilextra text-[32px] lg:text-[80px] mb-2">Case Studies</h1>
       <p className="lg:text-2xl font-gilmedium tracking-[1px] leading-6 text-[#5b5e5f] mb-8">
         We deliver a unique and truly bespoke approach to delivering
@@ -13,7 +17,7 @@ const Casestudy = () => {
       <div className="flex">
         <img className="w-[224px] h-[264px] md:h-[520px] md:w-[450px]" src={images.France.src} alt="" />
       </div>
-    </div>
+    </motion.div>
   );
 };
 
